994a26315cb4c226eb3f16e07d1daa96d0243e59
Charlie Mcghee <charliemcghee@Charlies-MacBook-Pro.local>
2021-01-30 11:20:33 +0000

    documented code

--- HMM.py
+++ HMM.py
@@ -4,10 +4,27 @@ import numpy as np
 from nltk.tokenize import word_tokenize
 
 class Model:
+    """
+    A class to implement a Hidden Markov Model POS tagger as described in Jurafsky and Martin's
+    'Speech and Language Processing'(p148-155). The draft of the 3rd edition can be found here:
+
+    https://web.stanford.edu/~jurafsky/slp3/ed3book.pdf
+
+    """
+
 
     def __init__(self, corpus):
-        #takes input from nltk.corpus, for example treebank or brown
+        """
+        The init function takes the tagged sentences in an NLTK corpus (specified by the corpus
+        parameter) and appends a START and END tag/word to each sentence.
 
+        :classattribute words_tags: list of tuples (word,tag) from input corpus
+        :classattribute tags: list of tags from self.words_tags
+        :classattribute uniquetags: set of unique tags from self.tags
+        :classattribute uniquewords: set of unique words from self.words_tags
+        :classattribute tagbigrams: list of bigrams of tags from self.tags
+
+        """
         self.words_tags = []
 
         for sent in corpus.tagged_sents(tagset = 'universal'):
@@ -21,7 +38,17 @@ class Model:
         self.tagbigrams = list(bigrams(self.tags))
 
     def train(self):
+        """
+        Function which creates a numpy matrix of the transition probabilities between states (tags)/
 
+        :classattribute tagdict: a dictionary which maps an index to each unique tag
+        :classattribute numdict: a dictionary which maps a tag to it's index from tagdict
+        :classattribute transition_prosb: numpy matrix which stores the transition probabilities
+        between states (tags)
+        :classattribute tagbigrams: list of bigrams of tags from self.tags
+        :classattribute uniquetags: set of unique tags from self.tags
+
+        """
 
         self.tagdict = {tag:num for (num, tag) in enumerate(self.uniquetags)}
         self.numdict = {num:tag for (tag, num) in self.tagdict.items()}
@@ -43,6 +70,15 @@ class Model:
 
 
     def emission_probs(self, w, t):
+        """
+        Function which returns the probability of a word being observed given a specific tag.
+
+        :param w: input word
+        :param t: input tag
+        :classattribute words_tags: list of tuples (words, tags) from input corpus
+        :return emission_prob: probability of a word being observed given a specific tag
+        """
+
         count_w_t = len([[word, tag] for [word, tag] in self.words_tags if tag == t\
                          and word == w]) + 1
         count_t = len([[tag] for [word, tag] in self.words_tags if tag == t])
@@ -51,6 +87,20 @@ class Model:
 
 
     def tagger(self, sentence):
+        """
+        Function which implements the Viterbi algorithm to find the highest probability tag
+        sequence.
+
+        :param sentence: input string representing the sentence to be tagged
+        :classattribute vitmatrix: numpy matrix which stores the probability values for the
+        Viterbi algorithm
+        :classattribute tagdict: a dictionary which maps an index to each unique tag
+        :classattribute transition_probs: numpy matrix which stores the transition
+        probabilities between states (tags)
+        :classattribute numdict: a dictionary which maps a tag to it's index from tagdict
+        :return backpointer: list of best tag sequence
+        """
+
         splitsent = word_tokenize(sentence)
         self.vitmatrix = np.zeros(shape = (len(self.tagdict), len(splitsent)), dtype = float)
 
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 <h1>HMM-POS-Tagger</h1>
 
 
+This is my first attempt at a POS tagger using a Hidden Markov Model. Usage would be the following:
 
-This is my first attempt at a POS tagger using a Hidden Markov Model.
- 
+>>> from nltk.corpus import brown
+>>> a = HMM.Model(brown).train()
+>>> a.tagger('Can you see the train?')
+['VERB', 'PRON', 'VERB', 'DET', 'NOUN', '.']
+
+
 
